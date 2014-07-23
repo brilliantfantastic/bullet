f54d7ed2e00787155bb698ce47d67b8f05e40e86
Jamie Wright <jamie@brilliantfantastic.com>
2014-07-23 02:25:49 -0400

    Add JVFloat component js and css

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('vendor/JVFloat/jvfloat.js');
+app.import('vendor/JVFloat/jvfloat.css');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
