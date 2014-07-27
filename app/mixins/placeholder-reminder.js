f029c545b2a5ab5967b74aa8dbf5d440c2aaa8c5
Jamie Wright <jamie@brilliantfantastic.com>
2014-07-27 16:35:08 -0400

    Display a header on sign up form

--- app/mixins/placeholder-reminder.js
+++ app/mixins/placeholder-reminder.js
@@ -2,7 +2,6 @@ import Ember from "ember";
 
 export default Ember.Mixin.create({
   didInsertElement: function() {
-    console.log("item has been inserted");
     return this.$().jvFloat();
   }
 });
