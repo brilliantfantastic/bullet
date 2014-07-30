e6ba6b4cd3f596cb5561ba4cb4c3569f07b5a4c3
Jamie Wright <jamie@brilliantfantastic.com>
2014-07-29 23:06:44 -0400

    Extend the signup controller with the UnprocessableEntityController

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,17 +1,18 @@
 import Ember from "ember";
+import UnprocessableEntityControllerMixin from "bullet/mixins/unprocessable-entity-controller";
 
-export default Ember.ObjectController.extend({
+export default Ember.ObjectController.extend(UnprocessableEntityControllerMixin, {
   actions: {
     signup: function() {
       var controller = this;
-      var session = this.get('session');
+      var session = this.get("session");
       this.get("model").save().then(function(user) {
-        var properties = { user_token: user.get('accessToken'), user_email: user.get('email') };
+        var properties = { user_token: user.get("accessToken"), user_email: user.get("email") };
         session.setup(BulletENV.authenticator, properties, true);
         controller.transitionToRoute("journal");
       }, function(error) {
         controller.get("model").transitionTo("created.uncommitted");
-        controller.set("errorMessage", error.errors[0]);
+        controller.showUnprocessableEntityError(error);
       });
     }
   }
