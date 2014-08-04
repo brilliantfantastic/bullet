175868c4ef74ae003c4a66bb8939f0a4797906f8
Jamie Wright <jamie@brilliantfantastic.com>
2014-08-04 08:55:57 -0400

    Stubbed out notebooks API call

---
+++ server/routes/notebook.js
@@ -0,0 +1,7 @@
+module.exports = function(app) {
+  app.get("/api/notebooks", function(req, res) {
+    var notebooks = { notebooks: [{ id: 1, page_ids: [1] }], pages: [{ id: 1, title: "Index" }] };
+    res.setHeader("Content-Type", "application/json");
+    res.send(200, notebooks);
+  });
+};
