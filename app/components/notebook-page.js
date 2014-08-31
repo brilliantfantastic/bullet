04332c44857c6f4c192956b5007b6fdaa58738f3
Jamie Wright <jamie@brilliantfantastic.com>
2014-08-31 13:25:14 -0400

    Moved page to a component

--- app/components/notebook-page.js
+++ app/components/notebook-page.js
@@ -1,10 +1,11 @@
 import Ember from "ember";
 import DefaultPageTitle from "bullet/utils/default-page-title";
 
-export default Ember.ObjectController.extend({
+export default Ember.Component.extend({
+  classNames: ['page'],
   actions: {
     defaultTitle: function() {
-      var page = this.get("model");
+      var page = this.get("page");
       if (Ember.isEmpty(page.get("title")) && !page.isTitleReadOnly) {
         page.set("title", DefaultPageTitle.create().get("title"));
       }
