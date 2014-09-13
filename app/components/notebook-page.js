b3a1f7f02c403d6ff7c58f03736f5168a0061846
Jamie Wright <jamie@brilliantfantastic.com>
2014-09-13 17:27:44 -0600

    Add bullets on page click

--- app/components/notebook-page.js
+++ app/components/notebook-page.js
@@ -3,11 +3,20 @@ import DefaultPageTitle from "bullet/utils/default-page-title";
 
 export default Ember.Component.extend({
   classNames: ['page'],
+  focusIn: function() {
+    var page = this.get("page");
+    this._actions['addBullet'].apply(this, [page]);
+  },
   actions: {
-    defaultTitle: function() {
-      var page = this.get("page");
+    defaultTitle: function(page) {
       if (Ember.isEmpty(page.get("title")) && !page.isReadOnly) {
         page.set("title", DefaultPageTitle.create().get("title"));
+      }
+    },
+    addBullet: function(page) {
+      if (!page.get("isReadOnly") && Ember.isEmpty(page.get("bullets")) && !Ember.isEmpty(page.get("title"))) {
+        var store = page.get("store");
+        page.get("bullets").pushObject(store.createRecord("bullet"));
       }
     }
   }
