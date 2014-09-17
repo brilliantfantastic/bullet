6a7a9e667a0997318317fe78c33adfdc4522271f
Jamie Wright <jamie@brilliantfantastic.com>
2014-09-16 23:27:01 -0400

    Bullet#type which returns the dasherized version of the type

--- app/models/bullet.js
+++ app/models/bullet.js
@@ -2,5 +2,8 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
   title: DS.attr("string"),
-  page: DS.belongsTo("page")
+  page: DS.belongsTo("page"),
+  type: function() {
+    return this.constructor.typeKey.dasherize();
+  }.property()
 });
--- tests/unit/models/task-bullet-test.js
+++ tests/unit/models/task-bullet-test.js
@@ -10,3 +10,7 @@ test("stores a due date", function() {
   var bullet = this.subject({dueDate: due});
   equal(bullet.get("dueDate"), due);
 });
+
+test("type is the dasherized version of the typeKey", function() {
+  equal(this.subject().get("type"), "task-bullet");
+});
