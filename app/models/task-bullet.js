caa9a3c3773baf46ad34a2d417f59b37f5d0cd3e
Jamie Wright <jamie@brilliantfantastic.com>
2014-09-17 23:44:21 -0400

    Create task bullet by default when a page is created

--- app/models/task-bullet.js
+++ app/models/task-bullet.js
@@ -1,4 +1,6 @@
+import DS from "ember-data";
 import Bullet from "bullet/models/bullet";
 
 export default Bullet.extend({
+  isComplete: DS.attr("boolean")
 });
--- app/views/bullet-action.js
+++ app/views/bullet-action.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+  classNames: ['bullet--action'],
   templateName: function() {
     var componentPath = "components/" + this.get("bullet.type") + "-action";
     return componentPath;
